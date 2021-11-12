acf6a1874d61a9719bd6b23b5cbce6e90cfdb6b6
michele <gmvalfre@yahoo.it>
2021-11-12 18:29:29 +0100

    updated

--- CharityWebsite/CharityWebsite/settings.py
+++ CharityWebsite/CharityWebsite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-ijx-9j5+9x_p@3mjzuyvj7&j_xsq#=e_(m4)o&^(lmh*h-vrty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+ "testsitewendover.herokuapp.com", "localhost", "127.0.0.1"
+]
 
 
 # Application definition
@@ -56,6 +58,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
 
 ROOT_URLCONF = 'CharityWebsite.urls'
 
@@ -125,7 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/home/michele/GitRepos/CharityWebsite/CharityWebsite/website/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
